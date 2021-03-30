30eba4210228bc1c3609b16425736a8ddf463591
Hadrian Hughes <hadrian.hughes@and.digital>
2021-03-30 20:17:36 +0100

    Return all fully qualified file paths including subdirectories

--- src/Input.hs
+++ src/Input.hs
@@ -4,8 +4,26 @@ module Input
 
 import System.Directory
 import Control.Monad
+import Debug.Trace
+
+isRealObject :: (String -> IO Bool) -> String -> String -> IO Bool
+isRealObject _ _ "."  = return False
+isRealObject _ _ ".." = return False
+isRealObject f path name = f (path ++ name)
 
 getFiles :: String -> IO [String]
 getFiles dir = do
-  files <- getDirectoryContents dir >>= filterM doesFileExist
-  return $ map ((dir ++ "/") ++) files
+  let fullDir = dir ++ "/"
+
+  contents <- getDirectoryContents dir
+  directories <- filterM (isRealObject doesDirectoryExist fullDir) contents
+
+  fs <- filterM (isRealObject doesFileExist fullDir) contents
+
+  deepFs <- if length directories == 0
+               then return []
+               else mapM getFiles $ map (fullDir ++) directories
+
+  let files = fs ++ (concat deepFs)
+
+  return $ map (fullDir ++) files
