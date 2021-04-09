513111083950832616c8d7e761a235572fb715ea
Hadrian Hughes <hadrian.hughes@and.digital>
2021-04-09 11:40:47 +0100

    Filter completed items

--- app/Main.hs
+++ app/Main.hs
@@ -14,12 +14,15 @@ initialise = do
   setupContext (parseArgs args)
 
 
-main :: IO [Bool]
+main :: IO [()]
 main = do
   ctx <- initialise
   files <- collectFiles ctx
   contents <- mapM readFile files
 
   let todos = concat $ map findTodos $ zip files contents
+  states <- mapM checkTodoDone todos
 
-  mapM checkTodoDone todos
+  let completed = [t | (t,s) <- zip todos states, s]
+
+  mapM (putStrLn . show) completed
