e9af26aae98575a302927eb3f7541dada8db705c
Hadrian Hughes <hadrian.hughes@and.digital>
2021-04-14 12:51:56 +0100

    Create Map for comment token types

--- src/Config.hs
+++ src/Config.hs
@@ -13,11 +13,26 @@ data AppContext = AppContext { path  :: FilePath
 
 data Todo = Todo FilePath (Integer,Integer) [String] deriving (Show)
 
+data CommentToken = CommentToken String (Maybe String)
+
 
 
 -- Directories starting with . don't need to be added
 ignoredDirectories :: Set String
 ignoredDirectories = Set.fromList ["node_modules"]
+
+
+commentTokens :: Map String [CommentToken]
+commentTokens = Map.fromList [(".hs", [smplCmnt "--"]),
+                              (".js", [smplCmnt "//", fllCmnt "/*" "*/"])]
+
+
+smplCmnt :: String -> CommentToken
+smplCmnt t = CommentToken t Nothing
+
+
+fllCmnt :: String -> String -> CommentToken
+fllCmnt t1 t2 = CommentToken t1 (Just t2)
 
 
 checkCompletedString :: String
