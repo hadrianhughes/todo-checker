77972073141938316089f6a9415f90e31a6e3bc1
Hadrian Hughes <hadrian.hughes@and.digital>
2021-04-16 14:03:40 +0100

    Create getStdin function for receiving piped input

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,22 @@
 module Main (main) where
 
 import System.Environment
+import System.IO
 import Control.Monad.State
 
 import Config
 import InputOutput
 import Utils
 import Branches
+
+
+getStdin :: IO (Maybe [String])
+getStdin =
+  do buffState <- hGetBuffering stdin
+     if buffState == LineBuffering
+        then return Nothing
+        else do contents <- getContents
+                return $ Just (lines contents)
 
 
 initialise :: IO (Action, AppContext)
